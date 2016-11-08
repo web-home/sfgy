dceb54956e594a4cb5f280953c50308afd150af9
H5zhangyi <154730619@qq.com>
2016-11-08 21:32:56 +0800

    网站头部更新

---
+++ app/js/header.js
@@ -0,0 +1,21 @@
+define(function(require,exports,module){
+	var Header = {
+		initPage: function(){
+			this.scFn();
+			this.menuFn();
+		},
+		scFn: function(){
+			$('.logo-rt .sc').click(function(){
+				alert('加入收藏失败，请使用Ctrl+D添加');
+			});
+		},
+		menuFn: function(){
+			$('.nav .top').hover(function(){
+				$(this).find('.sub').show();
+			},function(){
+				$(this).find('.sub').hide();
+			})
+		}
+	}
+	module.exports = Header;
+})
--- app/js/home.js
+++ app/js/home.js
@@ -1,7 +1,15 @@
 define(function(require,expores,module){
+	var header = require('../views/headerView.html');
+	var headerjs = require('../js/header.js');
+
 	var Home = {
 		initPage: function(){
-
+			this.showView();
+		},
+		showView: function(){
+			var _header = _.template( header );
+			$('#header').html( _header() );
+			headerjs.initPage();
 		}
 	}
 	module.exports = Home;
